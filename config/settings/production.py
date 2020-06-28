92223469618b623ddcb7b1554bdd21b6f4e85e29
AlanDavd <alandavidrl11@gmail.com>
2020-06-28 14:20:46 -0500

    Add production config

--- config/settings/production.py
+++ config/settings/production.py
@@ -66,7 +66,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [  # noqa F405
 # Email
 DEFAULT_FROM_EMAIL = env(
     'DJANGO_DEFAULT_FROM_EMAIL',
-    default='Comparte Ride <noreply@comparteride.com>'
+    default='Hobbify <noreply@hobbify.com>'
 )
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
 EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[Comparte Ride]')
